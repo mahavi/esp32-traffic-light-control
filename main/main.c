8dbafe5f103d692f2809f3a9aacbfa5ae0fd65f9
Maks Haviak <maks.haviak@pm.me>
2026-09-06 13:49:03 +0300

    Basic setup

--- main/main.c
+++ main/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+#include "driver/gpio.h"
+#include "driver/gptimer.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+static const char *TAG = "tlc";
+
+static void setup(void)
+{
+}
+
+static void loop(void)
+{
+  while(1)
+  {
+    vTaskDelay(pdMS_TO_TICKS(10));
+  }
+}
+
 void app_main(void)
 {
-
+  setup();
+  loop();
 }
